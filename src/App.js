7d03d7de3754885051fe6d079eda19072a44527a
roro1053 <c1053rroro@gmail.com>
2021-02-03 21:05:41 +0900

    カウントを表示

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
-function App() {
+function App(){
+  const [count, setCount] = useState(0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>You clicked {count} times.</p>
+      <button onClick={()=>setCount(count + 1)}>
+        click me
+      </button>
     </div>
-  );
+  )
 }
+
 
 export default App;
